91fb3dcda4f3dbb12cb98a087ebd2f62bcaef02b
Dominic Enns <1060350@bkrnet.de>
2026-05-18 08:43:54 +0200

    Überarbeitung der grund daten für connection

--- Apex_database/Program.cs
+++ Apex_database/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApexDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,8 +33,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<ApexDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
